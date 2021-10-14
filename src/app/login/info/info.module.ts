dcc40fe3798f9e9edef1fef43fc95a0fc80e57bc
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-10-14 15:58:04 -0300

    Adiciona componente info

---
+++ src/app/login/info/info.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { InfoComponent } from './info.component';
+
+
+@NgModule({
+  declarations: [
+    InfoComponent
+  ],
+  exports: [
+    InfoComponent
+  ],
+  imports: [
+  ],
+  providers: [],
+  bootstrap: []
+})
+
+export class InfoModule { }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { InfoModule } from './info/info.module';
 import { LoginComponent } from './login.component';
 import { LogoComponent } from './logo/logo.component';
 
@@ -12,6 +13,7 @@ import { LogoComponent } from './logo/logo.component';
     LoginComponent
   ],
   imports: [
+    InfoModule
   ],
   providers: [],
   bootstrap: []
