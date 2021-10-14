5a0c4b17ee06fbcfe13d931cef24f1fe993fc19e
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-10-14 15:37:32 -0300

    Adiciona o componente logo

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
+import { LogoComponent } from './logo/logo.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    LogoComponent
   ],
   exports: [
     LoginComponent
