41d010b3cc8f16efe786e91f76fe91a7a7412002
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-10-15 10:27:23 -0300

    Cria o componente header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { LoginModule } from './login/login.module';
   ],
   imports: [
     BrowserModule,
-    LoginModule
+    LoginModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
