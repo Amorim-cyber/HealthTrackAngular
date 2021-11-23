cc5094846daa9bd161619314e0269d51a2254477
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-11-23 20:23:03 -0300

    Separa os componentes de estilo de dashboard

--- src/app/shared/components/content/content-dashboard/content-dashboard.component.ts
+++ src/app/shared/components/content/content-dashboard/content-dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content-dashboard',
   templateUrl: './content-dashboard.component.html',
-  styleUrls: ['./content-dashboard.component.css']
+  styleUrls: ['./content-dashboard.component.css','./content-dashboard-graphs.component.css','./content-dashboard-circle.component.css']
 })
 export class ContentDashboardComponent implements OnInit {
 
